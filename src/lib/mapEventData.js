06f3038e9bfdbc179939557d2ad898654af0a720
ludrob <robin.ludovic@gmail.com>
2018-08-13 15:18:21 +0100

    working POC handlebars+js x-dash integration  🐿 v2.10.2

--- src/lib/mapEventData.js
+++ src/lib/mapEventData.js
@@ -3,22 +3,22 @@ const setDate = require('./event-date');
 
 module.exports = (theEvent, variant) => {
 	const eventUrl = new URL(theEvent.eventUrl);
-    const images = [...config.animationStaticImages, theEvent.imageUrl];
+	const images = [theEvent.imageUrl, ...config.animationStaticImages];
 	const showVariant = (variant === 'variant');
 
 	eventUrl.searchParams.set('segmentId', theEvent.segmentId);
 	eventUrl.searchParams.set('variant', showVariant.toString());
 
-    return {
-        id: theEvent.id,
-        eventTitle: theEvent.title,
-        mainImage: encodeURI(theEvent.imageUrl),
-        eventDate: setDate(theEvent),
-        eventUrl: eventUrl.toString(),
-        segmentId: theEvent.segmentId,
-        eventLocation: theEvent.location,
-        images,
-        showVariant,
-        strapline: theEvent.strapline
-    };
+	return {
+		dates: setDate(theEvent),
+		id: theEvent.id,
+		image1: images[0],
+		image2: images[1],
+		image3: images[2],
+		link: eventUrl.toString(),
+		location: theEvent.location,
+		segmentId: theEvent.segmentId,
+		showVariant,
+		title: theEvent.title
+	};
 };
