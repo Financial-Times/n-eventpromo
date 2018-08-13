8dc15c131459f61207cb9be77343613baac62d66
ludrob <robin.ludovic@gmail.com>
2018-08-13 13:53:49 +0100

    working handlebars and client examples  🐿 v2.10.2

--- src/lib/mapEventData.js
+++ src/lib/mapEventData.js
@@ -3,22 +3,22 @@ const setDate = require('./event-date');
 
 module.exports = (theEvent, variant) => {
 	const eventUrl = new URL(theEvent.eventUrl);
-	const images = [...config.animationStaticImages, theEvent.imageUrl];
+    const images = [...config.animationStaticImages, theEvent.imageUrl];
 	const showVariant = (variant === 'variant');
 
 	eventUrl.searchParams.set('segmentId', theEvent.segmentId);
 	eventUrl.searchParams.set('variant', showVariant.toString());
 
-	return {
-		id: theEvent.id,
-		eventTitle: theEvent.title,
-		mainImage: encodeURI(theEvent.imageUrl),
-		eventDate: setDate(theEvent),
-		eventUrl: eventUrl.toString(),
-		segmentId: theEvent.segmentId,
-		eventLocation: theEvent.location,
-		images,
-		showVariant,
-		strapline: theEvent.strapline
-	};
+    return {
+        id: theEvent.id,
+        eventTitle: theEvent.title,
+        mainImage: encodeURI(theEvent.imageUrl),
+        eventDate: setDate(theEvent),
+        eventUrl: eventUrl.toString(),
+        segmentId: theEvent.segmentId,
+        eventLocation: theEvent.location,
+        images,
+        showVariant,
+        strapline: theEvent.strapline
+    };
 };
