f9b2c2bc1431d6bafb92bfa39c81266968fadca4
ludrob <robin.ludovic@gmail.com>
2018-08-17 17:13:13 +0100

    code formatting, use array for images, use webpack to process demo files  🐿 v2.10.2

--- src/lib/mapEventData.js
+++ src/lib/mapEventData.js
@@ -12,9 +12,7 @@ module.exports = (theEvent, variant) => {
 	return {
 		dates: setDate(theEvent),
 		id: theEvent.id,
-		image1: images[0],
-		image2: images[1],
-		image3: images[2],
+		images,
 		link: eventUrl.toString(),
 		location: theEvent.location,
 		segmentId: theEvent.segmentId,
--- tests/lib/mapEventData.spec.js
+++ tests/lib/mapEventData.spec.js
@@ -13,15 +13,14 @@ describe('mapEventData()', () => {
 
 		expect(subject).toHaveProperty('id', anEvent.id);
 		expect(subject).toHaveProperty('title', anEvent.title);
-		expect(subject).toHaveProperty('image1', imageArrayFixture[0]);
-		expect(subject).toHaveProperty('image2', imageArrayFixture[1]);
-		expect(subject).toHaveProperty('image3', imageArrayFixture[2]);
+		expect(subject).toHaveProperty('images', imageArrayFixture);
 		expect(subject).toHaveProperty('link', expectedUrl.toString());
 		expect(subject).toHaveProperty('dates', '12 June 2018');
 		expect(subject).toHaveProperty('location', anEvent.location);
 		expect(subject).toHaveProperty('segmentId', anEvent.segmentId);
+		expect(subject).toHaveProperty('images');
 		expect(subject).toHaveProperty('showVariant', false);
-		//expect(subject.images).toEqual(expect.arrayContaining(imageArray));
+		expect(subject.images).toEqual(expect.arrayContaining(imageArrayFixture));
 	});
 
 	//Relevant to AB test probably ok to remove amend on conclusion.
@@ -34,10 +33,8 @@ describe('mapEventData()', () => {
 		expectedUrl.searchParams.set('variant', subject.showVariant);
 
 		expect(subject).toHaveProperty('showVariant', false);
-        expect(subject).toHaveProperty('image1', imageArrayFixture[0]);
-        expect(subject).toHaveProperty('image2', imageArrayFixture[1]);
-        expect(subject).toHaveProperty('image3', imageArrayFixture[2]);
-    });
+		expect(subject.images).toEqual(expect.arrayContaining(imageArrayFixture));
+	});
 
 	test('test provides the correct control properties', () => {
 
@@ -47,10 +44,8 @@ describe('mapEventData()', () => {
 		expectedUrl.searchParams.set('variant', subject.showVariant);
 
 		expect(subject).toHaveProperty('showVariant', true);
-        expect(subject).toHaveProperty('image1', imageArrayFixture[0]);
-        expect(subject).toHaveProperty('image2', imageArrayFixture[1]);
-        expect(subject).toHaveProperty('image3', imageArrayFixture[2]);
-    });
+		expect(subject.images).toEqual(expect.arrayContaining(imageArrayFixture));
+	});
 
 	test('test it sets date range if start and end dates differ', () => {
 		anEvent.scheduledEndTime = '2018-06-14T07:00:00.000Z';
