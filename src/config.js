6a0cadcf06534e1a27cdd79353f816ae595786da
Tye, Thurston <Thurston.Tye@ft.com>
2018-10-02 18:19:43 +0100

    user-driven-endpoint  🐿 v2.10.2

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const staticAssetsBasePath = 'https://www.ft.com/__assets/creatives/better-promo
 
 module.exports = {
 	'apiPath': '/eventpromo/api/get-one/',
+	'apiHistoryPath' : '/eventpromo/api/get-personal/',
 	'animationStaticImages': [
 		`${staticAssetsBasePath}/break_out.jpg`,
 		`${staticAssetsBasePath}/audiance_clapping.jpg`
