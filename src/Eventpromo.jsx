eb57ff814128f84f9febe637e59400b769b6cb51
Max Bladen-Clark <max@buzzard.dev>
2021-02-24 11:25:04 +0000

    feat(HIVE-43): remove animation from promo and replace 'images' prop with 'imageUrl'

--- src/Eventpromo.jsx
+++ src/Eventpromo.jsx
@@ -1,14 +1,14 @@
 import xEngine from '@financial-times/x-engine';
 
 import Details from './Details';
-import ImagesContainer from './ImagesContainer';
 import styles from './Eventpromo.scss';
+import Image from './Image';
 
 const Eventpromo = (props) => {
 	return (
 		<div className={styles.eventpromo} data-event-focus="" data-focus-concept={props.id}>
 			<Details {...props} />
-			<ImagesContainer {...props} />
+			<Image {...props} />
 		</div>
 	);
 };
