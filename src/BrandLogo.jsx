a388d657e65e5230c5a4f356c0608f6d8eaa4115
Max Bladen-Clark <max@buzzard.dev>
2021-05-19 15:59:30 +0100

    feat(HIVE-110): add support for Online Courses from the FT Board Director Programme

--- src/BrandLogo.jsx
+++ src/BrandLogo.jsx
@@ -1,21 +1,27 @@
 import xEngine from '@financial-times/x-engine';
-
 import styles from './BrandLogos.scss';
-import commonStyles from './styles/common.scss'
+import commonStyles from './styles/common.scss';
 
 export const FTLiveLogo = () => (
     <div className={`${styles['brand']} ${styles['brand--ft-live']}`}>
         Presented by
-        <span className={`${styles['brand__logo-alt-text']} ${commonStyles['visually-hidden']}`} >FT Live</span>
+        <span className={`${styles['brand__logo-alt-text']} ${commonStyles['visually-hidden']}`}>FT Live</span>
     </div>
-)
+);
 
 export const BDPLogo = () => (
     <div className={`${styles['brand']} ${styles['brand--ft-board-director-programme']}`}>
         Presented by
         <span className={`${styles['brand__logo-alt-text']} ${commonStyles['visually-hidden']}`}>FT Board Director Programme</span>
     </div>
-)
+);
+
+export const BDPLogoDark = () => (
+    <div className={`${styles['brand']} ${styles['brand--ft-board-director-programme-dark']}`}>
+        Presented by
+        <span className={`${styles['brand__logo-alt-text']} ${commonStyles['visually-hidden']}`}>FT Board Director Programme</span>
+    </div>
+);
 
 export const ForumsLogo = () => (
     <div className={`${styles['brand']} ${styles['brand--ft-forums']}`}>
