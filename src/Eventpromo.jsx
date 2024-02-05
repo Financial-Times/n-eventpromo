b87794fdfe7c2d55db6dbe44634bdc9fdc0be99c
Daniel Tonev <daniel.tonev@ft.com>
2024-02-05 13:52:09 +0200

    removed the explore all events button

--- src/Eventpromo.jsx
+++ src/Eventpromo.jsx
@@ -7,23 +7,12 @@ import {getBranding} from './utils/brands';
 
 const Eventpromo = ({...props}) => {
 	const branding = getBranding(props.brand || 'ft-live');
-	const eventsPageUrl = "https://events.ft.com/events-list";
 
 	return (
 		<div data-event-focus="" data-event-brand={props.brand} data-focus-concept={props.id}>
 			<div className={styles.eventpromo} >
 				<EventDetails {...branding} {...props}/>
 				<Image {...branding} {...props}/>
-			</div>
-
-			<div className={styles.eventpromo__btn__container}>
-				<a
-					className={styles.explore__btn}
-					href={eventsPageUrl}
-					data-trackable="community-explore-all-events-cta"
-				>
-					Explore all events
-				</a>
 			</div>
 		</div>
 	);
